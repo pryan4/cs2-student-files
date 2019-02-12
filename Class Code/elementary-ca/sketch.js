791e52ef75d6eca0c0510627074f26b8b3b48cc8
mdarfler <mdarfler@gmail.com>
2019-02-12 17:20:48 -0500

    eca

---
+++ Class Code/elementary-ca/sketch.js
@@ -0,0 +1,18 @@
+/* This is a starter template for a p5.js sketch. If you don't want to bother with creating a new one every time use this instead. 
+
+Always make a copy of the entire project folder before starting.*/
+
+//Your Name(s)
+//Project Name
+//Date
+
+/* A brief description of what the program is */
+
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+}
