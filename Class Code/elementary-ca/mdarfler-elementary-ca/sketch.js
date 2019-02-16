b799c824c1ce80b8f2f76aec7c5e8c50e19050a1
mdarfler <mdarfler@gmail.com>
2019-02-16 16:09:36 -0500

    finish with scroll

--- Class Code/elementary-ca/mdarfler-elementary-ca/sketch.js
+++ Class Code/elementary-ca/mdarfler-elementary-ca/sketch.js
@@ -1,28 +1,69 @@
-
 //Your Name(s)
 //Project Name
 //Date
 
 /* A brief description of what the program is */
 
-let gen1 = Array(57).fill(0);
-
+ruleset = [0, 1, 0, 1, 1, 0, 1, 0];
+let ca = [];
+const gensize = 201;
 let gridsize;
+
 function setup() {
     createCanvas(400, 400);
-    gen1[round(gen1.length/2)]=1
-    gridsize = width/gen1.length
-    console.log(gridsize)
+    gridsize = width / gensize
+
+    gen0 = Array(gensize).fill(0);
+    gen0[floor(gensize/2)] = 1;
+    ca.push(gen0);
+    console.log(ca)
+
 }
 
 function draw() {
-    background(255);
+    display();
+    generate();
+    scroll();
+}
 
-    for(let i = 0; i < gen1.length; i++){
-        if(gen1[i] == 1){
-            noStroke();
-            fill(0);
-            rect(i*gridsize,0,gridsize,gridsize)
+function generate(){
+    const thisgen = ca[ca.length-1]
+    let nextgen = Array(gensize).fill(0);
+
+    for(let i = 1; i < gensize - 1; i++){
+        nextgen[i] = rules(thisgen[i-1],thisgen[i],thisgen[i+1])
+    }
+
+    ca.push(nextgen)
+    //console.log(ca)
+}
+
+function display(){
+    background(255);
+    fill(0);
+    noStroke();
+    for(let i = 0; i  < ca.length; i++){
+        for(let j = 0; j < ca[i].length; j++){
+            if(ca[i][j] == 1) {
+                rect(j * gridsize, i * gridsize, gridsize, gridsize)
+            }
         }
     }
+}
+
+function scroll(){
+    if(ca.length*gridsize > height){
+        ca.shift();
+    }
+}
+function rules(a, b, c) {
+    if (a == 1 && b == 1 && c == 1) return ruleset[0];
+    if (a == 1 && b == 1 && c === 0) return ruleset[1];
+    if (a == 1 && b === 0 && c == 1) return ruleset[2];
+    if (a == 1 && b === 0 && c === 0) return ruleset[3];
+    if (a === 0 && b == 1 && c == 1) return ruleset[4];
+    if (a === 0 && b == 1 && c === 0) return ruleset[5];
+    if (a === 0 && b === 0 && c == 1) return ruleset[6];
+    if (a === 0 && b === 0 && c === 0) return ruleset[7];
+    return 0;
 }
