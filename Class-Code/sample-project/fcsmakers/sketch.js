23e5d7f9bc7c130c21175cbc802511c5c095bd45
mdarfler <mdarfler@gmail.com>
2019-02-16 22:20:22 -0500

    sample project

---
+++ Class-Code/sample-project/fcsmakers/sketch.js
@@ -0,0 +1,14 @@
+//Your Name(s)
+//Project Name
+//Date
+
+/* A brief description of what the program is */
+
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+}
