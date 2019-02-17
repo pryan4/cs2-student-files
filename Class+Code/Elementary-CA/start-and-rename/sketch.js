bcb7dbbb4c5466571007cc0f31eeafa2118c8395
Michael Darfler <33043742+Mr-Darfler@users.noreply.github.com>
2019-02-16 20:52:45 -0500

    test

---
+++ Class+Code/Elementary-CA/start-and-rename/sketch.js
@@ -0,0 +1,13 @@
+//Your Name(s)
+//Project Name
+//Date
+
+/* A brief description of what the program is */
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225);
+}
