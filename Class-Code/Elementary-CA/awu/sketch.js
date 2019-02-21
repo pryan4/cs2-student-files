b5ffc17b49dcd295b71c7b15de761068ce0b6a3f
yinchen wu <yinchenwu@yinchens-mbp.fcs.edu>
2019-02-21 09:12:41 -0500

    commit

--- Class-Code/Elementary-CA/awu/sketch.js
+++ Class-Code/Elementary-CA/awu/sketch.js
@@ -4,11 +4,27 @@
 
 /* A brief description of what the program is */
 
+let width = 400
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width, 400);
 }
 
 function draw() {
-    background(220);
+    background(200);
+}
+
+let list_length = 100
+let gen_0 = Array(list_length).fill(0)
+gen_0[Math.floor(list_length/2)]=1
+
+let cell_size = width/list_lengthcell
+
+gen = 0
+
+for(let i =0; list_length; i++){
+    if(gen_0[i] == 1){
+        rect(i*cell_size, gen, cellsize, cellsize)
+    }
+
 }
