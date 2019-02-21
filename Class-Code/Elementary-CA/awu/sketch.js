be3b27e0ec8d5d11a6e69c776f2a5a768e1a53b9
yinchen wu <yinchenwu@yinchens-mbp.fcs.edu>
2019-02-21 17:32:28 -0500

    Ente

--- Class-Code/Elementary-CA/awu/sketch.js
+++ Class-Code/Elementary-CA/awu/sketch.js
@@ -4,27 +4,28 @@
 
 /* A brief description of what the program is */
 
-let width = 400
-
-function setup() {
-    createCanvas(width, 400);
-}
-
-function draw() {
-    background(200);
-}
-
-let list_length = 100
+let cell_size
+let list_length = 101
 let gen_0 = Array(list_length).fill(0)
 gen_0[Math.floor(list_length/2)]=1
 
-let cell_size = width/list_lengthcell
+function setup() {
+    createCanvas(400, 400);
+    cell_size=width/list_length;
+    console.log(cell_size)
+}
 
 gen = 0
 
-for(let i =0; list_length; i++){
-    if(gen_0[i] == 1){
-        rect(i*cell_size, gen, cellsize, cellsize)
-    }
+function draw() {
+    background(200);
 
+for(let i =0; i < list_length; i++){
+    if(gen_0[i] == 1){
+        noStroke();
+        fill(0);
+        rect(i*cell_size, gen, cell_size, cell_size)
+    }
 }
+}
+
