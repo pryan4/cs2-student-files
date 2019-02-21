a1169ce7f6266e5262a41f19fa6553edce065647
BLehrer <Benjaminrlehrer@gmail.com>
2019-02-21 09:10:33 -0500

    day 1

--- Class-Code/Elementary-CA/blehrer/sketch.js
+++ Class-Code/Elementary-CA/blehrer/sketch.js
@@ -1,14 +1,25 @@
-//Your Name(s)
-//Project Name
+//Ben Lehrer
+//Elementary-CA
 //Date
 
 /* A brief description of what the program is */
 
+let gen = [];
+let genNum = 0;
+let cellSize;
 
 function setup() {
     createCanvas(400, 400);
+
+    gen[genNum] = Array(101).fill(0);
+    gen[genNum][floor(gen[genNum].length / 2)] = 1;
+
+    cellSize = width / gen[genNum].length;
 }
 
 function draw() {
+    for(let i = 0; i < gen[genNum].length; ++i){
+
+    }
     background(220);
 }
