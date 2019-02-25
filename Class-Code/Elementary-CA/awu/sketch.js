5200128797ff8861138a2feed445f3455538ac21
yinchen wu <yinchenwu@yinchens-mbp.fcs.edu>
2019-02-25 14:28:06 -0500

    final version

--- Class-Code/Elementary-CA/awu/sketch.js
+++ Class-Code/Elementary-CA/awu/sketch.js
@@ -1,9 +1,9 @@
 //Your Name(s) Alex Wu
-//Project Name
-//Date
+//Project Name Elementary CA
+//Date FEB 25
 
-/* A brief description of what the program is */
-
+/* A brief description of what the program is 
+to mimic the CA operation by javascript*/
 const rules = [0, 1, 0, 1, 1, 0, 1, 0]
 var m = 0
 let ca = []
@@ -41,86 +41,13 @@ function draw() {
     m++
 }
 
-/*function draw() {
-    background(200);
-    for (let i = 0; i < list_length; i++) {
-
-        if (gen_0[i] == 1) {
-            noStroke();
-            fill(0);
-            rect(i * cell_size, 0, cell_size, cell_size)
-        }
-    }
-
-    let gen_new
-    for (let k = 1; k < 400; k++) {
-        gen_new = Array(list_length).fill(0);
-        gen_new[0] = gen_0[0];
-        gen_new[list_length - 1] = gen_0[list_length - 1];
-        for (let j = 1; j < list_length - 1; j++) {
-            gen_new[j] = ruleset(gen_0[j - 1], gen_0[j], gen_0[j + 1])
-        }
-        ca.push(gen_new);
-        gen_0 = gen_new;
-
-        for (let i = 0; i < list_length; i++) {
-            if (ca[k][i] == 1) {
-                noStroke();
-                fill(0);
-                rect(i * cell_size, k * cell_size, cell_size, cell_size)
-            }
-        }
-    }
-
-}
-*/
-
 function ruleset(uno, dos, tres) {
-    if (uno == 1, dos == 1, tres == 1) return rules[0]
-    else if (uno == 1, dos == 1, tres == 0) return rules[1]
-    else if (uno == 1, dos == 0, tres == 1) return rules[2]
-    else if (uno == 1, dos == 0, tres == 0) return rules[3]
-    else if (uno == 0, dos == 1, tres == 1) return rules[4]
-    else if (uno == 0, dos == 1, tres == 0) return rules[5]
-    else if (uno == 0, dos == 0, tres == 1) return rules[6]
-    else if (uno == 0, dos == 0, tres == 0) return rules[7];
+    if (uno == 1 && dos == 1 && tres == 1) {return 0}
+    else if (uno == 1 && dos == 1 && tres == 0) {return 1}
+    else if (uno == 1 && dos == 0 && tres == 1) {return 0}
+    else if (uno == 1 && dos == 0 && tres == 0) {return 1}
+    else if (uno == 0 && dos == 1 && tres == 1) {return 1}
+    else if (uno == 0 && dos == 1 && tres == 0) {return 0}
+    else if (uno == 0 && dos == 0 && tres == 1) {return 1}
+    else if (uno == 0 && dos == 0 && tres == 0) {return 0}
 }
-
-/*
-let gen_new
-for(let k = 1; k < 400; k++) {
-    gen_new = Array(list_length).fill(0);
-    gen_new[0]=gen_0[0];
-    gen_new[list_length-1]=gen_0[list_length-1];
-    for(let j =1; j < list_length -1; j++){gen_new[j]=ruleset(gen_0[j-1],gen_0[j],gen_0[j+1])}
-    ca.push(gen_new);
-    gen_0 = gen_new ;}
-
-function draw() {
-    background(200);       
-    for(let i =0; i < list_length; i++){
-    
-        if(ca[6][i] == 1){
-            noStroke();
-            fill(0);
-            rect(i*cell_size, 0, cell_size, cell_size)}}}
-*/
-
-
-/*
-let gen_new
-for(let k = 1; k < 400; k++) {
-gen_new = Array(list_length).fill(0);
-gen_new[0]=gen_0[0];
-gen_new[list_length-1]=gen_0[list_length-1];
-for(let j =1; j < list_length -1; j++){gen_new[j]=ruleset(gen_0[j-1],gen_0[j],gen_0[j+1])}
-ca.push(gen_new);
-gen_0 = gen_new ;
-
-for(let i =0; i < list_length; i++){
-    if(gen_new[i] == 1){
-        noStroke();
-        fill(0);
-        rect(i*cell_size, k*cell_size, cell_size, cell_size)
-}}}
-*/
