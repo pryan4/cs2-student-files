6e5135f69efe9e2353606ae42a20304c409009b6
yinchen wu <yinchenwu@yinchens-mbp.fcs.edu>
2019-02-26 13:17:59 -0500

    final final version!

--- Class-Code/Elementary-CA/awu/sketch.js
+++ Class-Code/Elementary-CA/awu/sketch.js
@@ -11,13 +11,18 @@ let cell_size
 let list_length = 101
 let gen_0 = Array(list_length).fill(0)
 gen_0[Math.floor(list_length / 2)] = 1
+let resetButton
 
 function setup() {
-    createCanvas(400, 4000);
+    createCanvas(400, 400);
     cell_size = width / list_length;
     console.log(cell_size)
+    resetButton = createButton('reset')
+    resetButton.mousePressed(() => console.log('Hello'))
+    ca.push(gen_0)
 }
 
+/*
 function draw() {
     
     for (let i = 0; i < list_length; i++) {
@@ -40,14 +45,60 @@ function draw() {
     gen_0 = gen_new 
     m++
 }
+*/
+
+function draw() {
+    background(255)
+    if (ca.length * cell_size > width) {
+        ca.shift()
+    }
+
+    for (let m = 0; m < ca.length; m++) {
+        for (let i = 0; i < list_length; i++) {
+
+            if (ca[m][i] == 1) {
+                noStroke();
+                fill(0);
+                rect(i * cell_size, m * cell_size, cell_size, cell_size)
+            }
+        }
+    }
+    let gen_old = ca[ca.length - 1]
+    let gen_new = Array(list_length).fill(0);
+    gen_new[0] = gen_old[0];
+    gen_new[list_length - 1] = gen_old[list_length - 1];
+
+    for (let j = 1; j < list_length - 1; j++) {
+        gen_new[j] = ruleset(gen_old[j - 1], gen_old[j], gen_old[j + 1])
+    }
+    ca.push(gen_new)
+
+    
+}
+
+
+
+
+
+
+
 
 function ruleset(left, middle, right) {
-    if (left == 1 && middle == 1 && right == 1) {return 0}
-    else if (left == 1 && middle == 1 && right == 0) {return 1}
-    else if (left == 1 && middle == 0 && right == 1) {return 0}
-    else if (left == 1 && middle == 0 && right == 0) {return 1}
-    else if (left == 0 && middle == 1 && right == 1) {return 1}
-    else if (left == 0 && middle == 1 && right == 0) {return 0}
-    else if (left == 0 && middle == 0 && right == 1) {return 1}
-    else if (left == 0 && middle == 0 && right == 0) {return 0}
+    if (left == 1 && middle == 1 && right == 1) {
+        return 0
+    } else if (left == 1 && middle == 1 && right == 0) {
+        return 1
+    } else if (left == 1 && middle == 0 && right == 1) {
+        return 0
+    } else if (left == 1 && middle == 0 && right == 0) {
+        return 1
+    } else if (left == 0 && middle == 1 && right == 1) {
+        return 1
+    } else if (left == 0 && middle == 1 && right == 0) {
+        return 0
+    } else if (left == 0 && middle == 0 && right == 1) {
+        return 1
+    } else if (left == 0 && middle == 0 && right == 0) {
+        return 0
+    }
 }
