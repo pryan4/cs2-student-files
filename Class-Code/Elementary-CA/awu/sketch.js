6210c9c2bbdb39c66697af54d9d651a3fc751696
yinchen wu <yinchenwu@yinchens-mbp.fcs.edu>
2019-03-01 11:04:52 -0500

    final final version

--- Class-Code/Elementary-CA/awu/sketch.js
+++ Class-Code/Elementary-CA/awu/sketch.js
@@ -4,54 +4,33 @@
 
 /* A brief description of what the program is 
 to mimic the CA operation by javascript*/
-const rules = [0, 0, 0, 1, 1, 1, 1, 0]
-var m = 0
+const rules = [0, 0, 0, 1, 1, 0, 1, 1]
 let ca = []
 let cell_size
 let list_length = 201
 let gen_0 = Array(list_length).fill(0)
 gen_0[Math.floor(list_length / 2)] = 1
 let resetButton
+let CA = []
+
 
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(400, 400);
     cell_size = width / list_length;
     console.log(cell_size)
     resetButton = createButton('reset')
-    resetButton.mousePressed(() => console.log('Hello'))
+    resetButton.mousePressed(restart)
     ca.push(gen_0)
-}
+    box = createInput('',text);
+}    
 
+function restart() {
+    ca = []
+    ca.push(gen_0);
+}
 /*
 function draw() {
-    
-    for (let i = 0; i < list_length; i++) {
-
-        if (gen_0[i] == 1) {
-            noStroke();
-            fill(0);
-            rect(i * cell_size, m * cell_size, cell_size, cell_size)
-        }
-    }
-    ca.push(gen_0);
-
-    let gen_new = Array(list_length).fill(0);
-    gen_new[0] = gen_0[0];
-    gen_new[list_length - 1] = gen_0[list_length - 1];
-
-    for (let j = 1; j < list_length - 1; j++) {
-        gen_new[j] = ruleset(gen_0[j - 1], gen_0[j], gen_0[j + 1])
-    }
-    gen_0 = gen_new 
-    m++
-}
-*/
-
-function draw() {
     background(255)
-    if (ca.length * cell_size > width) {
-        ca.shift()
-    }
 
     for (let m = 0; m < ca.length; m++) {
         for (let i = 0; i < list_length; i++) {
@@ -73,7 +52,43 @@ function draw() {
     }
     ca.push(gen_new)
 
-    
+    if (ca.length * cell_size > width) {
+        ca.shift()
+    }
+
+}
+
+*/
+
+
+
+function draw() {
+    background(255)
+
+    for (let m = 0; m < ca.length; m++) {
+        for (let i = 0; i < list_length; i++) {
+
+            if (ca[m][i] == 1) {
+                noStroke();
+                fill(0);
+                rect(i * cell_size, m * cell_size, cell_size, cell_size)
+            }
+        }
+    }
+    let gen_old = ca[ca.length - 1]
+    let gen_new = Array(list_length).fill(0);
+    gen_new[0] = gen_old[0];
+    gen_new[list_length - 1] = gen_old[list_length - 1];
+
+    for (let j = 1; j < list_length - 1; j++) {
+        gen_new[j] = ruleset(gen_old[j - 1], gen_old[j], gen_old[j + 1])
+    }
+    ca.push(gen_new)
+
+    if (ca.length * cell_size > width) {
+        ca.shift()
+    }
+
 }
 
 
