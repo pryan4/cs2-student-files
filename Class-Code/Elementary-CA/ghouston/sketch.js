bb1a15fbee837b98b20029506ad875b8fa6b8b02
g_that_boul <g_that_boul@Admins-MacBook-Air.local>
2019-03-11 11:52:28 -0400

    Final Code

--- Class-Code/Elementary-CA/ghouston/sketch.js
+++ Class-Code/Elementary-CA/ghouston/sketch.js
@@ -9,31 +9,30 @@ function setup() {
   gen0[floor(gensize/2)]=1;
   ca.push(gen0);
   gridsize = width/gensize
+  fill(0);
 }
 
 function draw() {
-  
-    //display
-    noStroke();
-    fill(0);
-    for(let j = 0; j < ca.length; j++)
-        for(let i = 0; i < gensize; i++){
-            if(ca[j][i] == 1){
-                rect(i*gridsize,j*gridsize,gridsize);
-            }
-    }
+  background(255)
+  //display
+  noStroke();
+  for(let j = 0; j < ca.length; j++)
+      for(let i = 0; i < gensize; i++){
+          if(ca[j][i] == 1){
+            rect(i*gridsize,j*gridsize,gridsize,gridsize);
+          }
+  }
 
     //generate
-    let thisGen = ca[ca.length-1]
-    let nextGen = Array(gensize).fill(0);
+  let thisGen = ca[ca.length-1]
+  let nextGen = Array(gensize).fill(0);
 
 
-    for(let i = 1; i < nextGen.length - 1; i++){
-      let nextval = applyRules(thisGen[i],
-        thisGen[i+1]);
-        nextGen[i]= nextval
-    }
-    ca.push(nextGen);
+  for(let i = 1; i < nextGen.length - 1; i++){
+    let nextval = applyRules( thisGen[i-1],thisGen[i],thisGen[i+1]);
+      nextGen[i]= nextval
+  }
+  ca.push(nextGen);
 
   }
 
@@ -52,6 +51,3 @@ function draw() {
 
   //   for(let i = 0; i < gen1.length; i++){
   //       if(gen1[i] == 1){
-    
-  }
-  }
