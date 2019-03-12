e0aac0576a3755f58af00cc0305c5da973fc9639
Pat Ryan <patryan@Pats-MacBook-Pro.local>
2019-03-12 11:04:54 -0400

    Baseline Code

--- Class-Code/Elementary-CA/pryan/sketch.js
+++ Class-Code/Elementary-CA/pryan/sketch.js
@@ -5,56 +5,56 @@
 /An Elementary Cellular Automata/
 
 let gensize = 61
-let indexVal = Math.floor(gensize/2);
-const ruleset =[0,1,0,1,1,0,1,0];
+let middleIndexVal = Math.floor(gensize / 2);
+const ruleset = [0, 1, 0, 1, 1, 0, 1, 0];
 let gen1 = Array(gensize).fill(0);
- gen1[indexVal]=1;
-let ca =[];
+gen1[middleIndexVal] = 1;
+let ca = [];
 let gridsize;
+
 function setup() {
     createCanvas(800, 400);
-    gridsize = width/gen1.length
-    console.log(gridsize)
+    gridsize = width / gen1.length
     ca.push(gen1)
+    
 }
 
 function draw() {
     background(255);
     noStroke();
     fill(0);
-    for(let j = 0; j < ca.length; j++){
-        
-    
-    for(let i = 0; i < gensize; i++){
-        if(ca[j][i]== 1 ){
-            rect(i*gridsize,j*gridsize,gridsize,gridsize);
+    for (let j = 0; j < ca.length; j++) {
+
+
+        for (let i = 0; i < gensize; i++) {
+            if (ca[j][i] == 1) {
+                rect(i * gridsize, j * gridsize, gridsize, gridsize);
+            }
         }
     }
-}
-let thisGen = ca[ca.length-1]
-let nextGen = Array(gensize).fill(0)
+    let thisGen = ca[ca.length - 1]
+    let nextGen = Array(gensize).fill(0)
 
-for(let i = 1; i < nextGen.length - 1; i++){
-    let nextval = applyRules(thisGen[i-1],thisGen[i],thisGen[i+1]);
-    nextGen[i] = nextval
-}
-ca.push(nextGen);
+    for (let i = 1; i < nextGen.length - 1; i++) {
+        let nextval = applyRules(thisGen[i - 1], thisGen[i], thisGen[i + 1]);
+        nextGen[i] = nextval
+    }
+    ca.push(nextGen);
 
-console.log(nextGen)
 
-//video: Generate :: 13:01
+
 
 }
 
-function applyRules(l,me,r){
-    if(l == 1 && me == 1 && r == 1) return ruleset[0];
-    else if(l == 1 && me == 1 && r == 0) return ruleset[1];
-    else if(l == 1 && me == 0 && r == 1) return ruleset[2];
-    else if(l == 1 && me == 0 && r == 0) return ruleset[3];
-    else if(l == 0 && me == 1 && r == 1) return ruleset[4];
-    else if(l == 0 && me == 1 && r == 0) return ruleset[5];
-    else if(l == 0 && me == 0 && r == 1) return ruleset[6];
-    else if(l == 0 && me == 0 && r == 0) return ruleset[7];
-    
+function applyRules(l, me, r) {
+    if (l == 1 && me == 1 && r == 1) return ruleset[0];
+    else if (l == 1 && me == 1 && r == 0) return ruleset[1];
+    else if (l == 1 && me == 0 && r == 1) return ruleset[2];
+    else if (l == 1 && me == 0 && r == 0) return ruleset[3];
+    else if (l == 0 && me == 1 && r == 1) return ruleset[4];
+    else if (l == 0 && me == 1 && r == 0) return ruleset[5];
+    else if (l == 0 && me == 0 && r == 1) return ruleset[6];
+    else if (l == 0 && me == 0 && r == 0) return ruleset[7];
+
 
 }
