53de97734d52fae6bd0110586151734c27da9d19
BLehrer <Benjaminrlehrer@gmail.com>
2019-03-12 11:13:13 -0400

    added buttons and features

--- Class-Code/Game of Life/blehrer/sketch.js
+++ Class-Code/Game of Life/blehrer/sketch.js
@@ -7,18 +7,34 @@
 let cellSize;
 const cellCount = 200;
 
+let started = false;
+let startButton;
+
+let paused = false;
+let pauseButton;
+
+let resetButton;
+
 const backgroundColor = 220;
 
 let ca = new Array(cellCount);
 
 function setup() {
     createCanvas(600, 600);
-    //noStroke();
-    strokeWeight(0.5);
+    background(backgroundColor);
     noStroke();
     fill(0);
 
     frameRate(10);
+
+    startButton = createButton('CLICK HERE TO BEGIN');
+    startButton.mousePressed(initCA);
+
+    pauseButton = createButton('PAUSE');
+    pauseButton.mousePressed(pauseCA);
+    
+    resetButton = createButton('RESET');
+    resetButton.mousePressed(resetCA);
 
     cellSize = width / cellCount;
 
@@ -26,32 +42,80 @@ function setup() {
         ca[i] = new Array(cellCount).fill(0);
     }
 
-    for (let i = 1; i < cellCount - 1; ++i) {
-        for (let j = 1; j < cellCount - 1; ++j) {
-            ca[i][j] = random() < 0.5 ? 1 : 0;
+    textAlign(CENTER, CENTER);
+    textSize(20);
+    text("CLICK AND DRAG MOUSE OVER CELLS TO INIT", width / 2, height / 2);
+}
+
+function draw() {
+    if (!started) {
+        if (mouseIsPressed) {
+            let cellX = floor(map(mouseX, 0, width, 0, cellCount));
+            let cellY = floor(map(mouseY, 0, width, 0, cellCount));
+            ca[cellX][cellY] = 1;
+            drawCA();
+        }
+        return;
+    }
+
+    if (paused)
+        return;
+
+    drawCA();
+
+    if (checkExtinction()) {
+        text("ALL CELLS ARE DEAD \n CLICK AND DRAG TO RESTART", width / 2, height / 2);
+        started = false;
+        return;
+    }
+
+    calcNewCA();
+}
+
+function resetCA() {
+    for (let i = 0; i < cellCount; ++i) {
+        ca[i].fill(0);
+    }
+    started = false;
+    background(backgroundColor);
+    text("CLICK AND DRAG MOUSE OVER CELLS TO INIT", width / 2, height / 2);
+}
+
+function checkExtinction() {
+    for (let i = 0; i < cellCount; ++i) {
+        for (let j = 0; j < cellCount; ++j) {
+            if (ca[i][j] == 1)
+                return false;
+        }
+    }
+    return true;
+}
+
+function initCA() {
+    started = true;
+}
+
+function pauseCA() {
+    paused = !paused;
+}
+
+function drawCA() {
+    background(backgroundColor);
+    for (let i = 0; i < cellCount; ++i) {
+        for (let j = 0; j < cellCount; ++j) {
+            if (ca[i][j] == 1) rect(i * cellSize, j * cellSize, cellSize, cellSize);
         }
     }
 }
 
-function draw() {
-    background(backgroundColor);
-
-    for (let i = 0; i < cellCount; ++i) {
-        for (let j = 0; j < cellCount; ++j) {
-            ca[i][j] == 1 ? fill(0) : fill(backgroundColor);
-            rect(i * cellSize, j * cellSize, cellSize, cellSize);
-        }
-    }
-
+function calcNewCA() {
     let oldCA = new Array(cellCount);
-
     for (let i = 0; i < cellCount; ++i) {
         oldCA[i] = new Array(cellCount);
         for (let j = 0; j < cellCount; ++j) {
             oldCA[i][j] = ca[i][j];
         }
     }
-
     for (let i = 1; i < cellCount - 1; ++i) {
         for (let j = 1; j < cellCount - 1; ++j) {
             ca[i][j] = applyRule(oldCA, i, j);
