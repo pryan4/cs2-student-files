9e6f08fed1d83d4d7cfde7dbbba753d4038eb9c8
BLehrer <Benjaminrlehrer@gmail.com>
2019-04-10 09:11:27 -0400

    bubbles can eat bubbles

--- Class-Code/OOP/blehrer/sketch.js
+++ Class-Code/OOP/blehrer/sketch.js
@@ -13,26 +13,47 @@
 */
 
 class Bubble {
-    constructor(_x, _y, _r) {
+    constructor(_x, _y, _r, _speed, _color) {
         this.x = _x;
         this.y = _y;
         this.r = _r;
+        this.angle = random(0, 2 * PI);
+        this.speed = _speed;
+        this.color = _color;
     }
     show() {
-        fill(255);
+        push();
+        colorMode(HSB);
+        fill(this.color, 255, 255);
         ellipse(this.x, this.y, this.r)
+        pop();
     }
     move() {
-        this.x += random(-width / 100, width / 100);
-        this.y += random(-height / 100, height / 100);
+        this.x += this.speed * cos(this.angle);
+        this.y += this.speed * sin(this.angle);
+        this.angle += random(-PI / 6, PI / 6);
     }
-    pop() {
-        if(this.x >= width || this.x <= 0) {
-            this.r = 0;
+    edge() {
+        if (this.x >= width || this.x <= 0) {
+            this.angle *= -1;
             return;
         }
-        if(this.y >= height || this.y <= 0) {
-            this.r = 0;
+        if (this.y >= height || this.y <= 0) {
+            this.angle *= -1;
+        }
+    }
+    touch(index) {
+        for (let i = 0; i < bubbles.length; ++i) {
+            if (i == index) continue;
+            if (dist(this.x, this.y, bubbles[i].x, bubbles[i].y) < this.r + bubbles[i].r) {
+                if (this.r > bubbles[i].r) {
+                    this.r += bubbles[i].r;
+                    bubbles[i].r = 0;
+                } else {
+                    bubbles[i].r += this.r;
+                    this.r = 0;
+                }
+            }
         }
     }
 }
@@ -40,19 +61,20 @@ class Bubble {
 let bubbles = new Array(2);
 
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(600, 600);
     noStroke();
-
-    bubbles[0] = new Bubble(random(0, width), random(0, height), random(1, 20));
-    bubbles[1] = new Bubble(random(0, width), random(0, height), random(1, 20));
+    for (let i = 0; i < 15; ++i) {
+        bubbles[i] = new Bubble(random(0, width), random(0, height), random(5, 30), random(1, 3), random(0, 360));
+    }
 }
 
 function draw() {
     background(220);
-    
-    for(let i = 0; i < bubbles.length; ++i){
+
+    for (let i = 0; i < bubbles.length; ++i) {
         bubbles[i].move();
-        bubbles[i].pop();
+        bubbles[i].edge();
+        bubbles[i].touch(i);
         bubbles[i].show();
     }
 }
